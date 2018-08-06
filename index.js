0949a06961e4f51d211dc7f2adba22e631ec0e31
QRoger1 <40796685+QRoger1@users.noreply.github.com>
2018-08-06 13:02:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Game = require('./src/game.js')
+app.get('/game',(req, res) => {
+  Game.join(req.query.token)
+  .then(game => {
+      res.send(game)
+    })
+  .catch(error => console.error(error))
+});
+
+app.post('/game', (req, res) => {
+  Game.create(req.body)
+    .then(game => {
+      res.send(game)
+    })
+    .catch(error => console.error(error))
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+});
